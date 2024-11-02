24b492aa188eab90dddccef9c77a61ac49dde9dd
Majewski Oliwier <majewskioliwierr@gmail.com>
2024-11-02 11:58:14 +0100

    Revert "Squash"
    This reverts commit d2211f2e7ae2097b2cf4499e99ea1bba609f6ced.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! branch6 x3");
+        System.out.println("Hello world! branch5 x3");
 
     }
 }
