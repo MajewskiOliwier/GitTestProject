8b88fc975c124ca1a4183ebdce2be51732bc4fac
Majewski Oliwier <majewskioliwierr@gmail.com>
2024-11-02 14:13:33 +0100

    commit master 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! MASTER 5");
+        System.out.println("Hello world! MASTER 7");
 
     }
 }
