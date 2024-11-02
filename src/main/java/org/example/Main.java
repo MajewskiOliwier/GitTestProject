b810790ae641d09517c2317c3a77fc38fcf31d85
Majewski Oliwier <majewskioliwierr@gmail.com>
2024-11-02 11:15:00 +0100

    commit 15

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,9 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! br32x");
-        System.out.println("Hello world! master");
-        System.out.println("Hello world! subject");
+        System.out.println("Hello world! branch4");
 
     }
 }
