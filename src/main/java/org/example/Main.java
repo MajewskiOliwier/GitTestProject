1e9f146023dfea8b59aec580caf8d70bb03462e2
Majewski Oliwier <majewskioliwierr@gmail.com>
2024-11-02 13:08:08 +0100

    commit br 14 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! br 14");
+        System.out.println("Hello world! br 14 2");
 
     }
 }
