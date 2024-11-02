c5fe8c69735bf1524219453e8b063e081d21709b
MajewskiOliwier <126395061+MajewskiOliwier@users.noreply.github.com>
2024-11-02 11:13:04 +0100

    Squash branch 3 nr 2 (#3)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,9 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hello world! br32x");
         System.out.println("Hello world! master");
+        System.out.println("Hello world! subject");
 
     }
 }
